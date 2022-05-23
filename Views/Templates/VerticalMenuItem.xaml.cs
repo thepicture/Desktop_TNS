face8b2f4f8380e6d47d70d6ddbfebf2a41ed44c
thepicture <85333076+thepicture@users.noreply.github.com>
2022-05-23 14:33:38 +0500

    feat(NavigationView): implement the vertical menu

---
+++ Views/Templates/VerticalMenuItem.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TelekomNevaSvyazWpfApp.Views.Templates
+{
+    /// <summary>
+    /// Interaction logic for VerticalMenuItem.xaml
+    /// </summary>
+    public partial class VerticalMenuItem : UserControl
+    {
+
+
+        public string MenuContent
+        {
+            get { return (string)GetValue(MenuContentProperty); }
+            set { SetValue(MenuContentProperty, value); }
+        }
+
+        public static readonly DependencyProperty MenuContentProperty =
+            DependencyProperty.Register("MenuContent", typeof(string), typeof(VerticalMenuItem), new PropertyMetadata(default(string)));
+
+
+
+        public byte[] Source
+        {
+            get { return (byte[])GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty SourceProperty =
+            DependencyProperty.Register("Source", typeof(byte[]), typeof(VerticalMenuItem), new PropertyMetadata(default(byte[])));
+
+
+        public VerticalMenuItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Views/UserControls/VerticalMenu.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using TelekomNevaSvyazWpfApp.Views.Templates;
+
+namespace TelekomNevaSvyazWpfApp.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for VerticalMenu.xaml
+    /// </summary>
+    public partial class VerticalMenu : UserControl
+    {
+        public VerticalMenu()
+        {
+            InitializeComponent();
+            foreach (string imagePath in Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory +  "/../../../Resources/MenuIcons"))
+            {
+                byte[] imageBytes = File.ReadAllBytes(imagePath);
+                FrameworkElementFactory menuItemFactory = new(
+                    typeof(VerticalMenuItem));
+                menuItemFactory.SetValue(VerticalMenuItem.SourceProperty, imageBytes);
+                menuItemFactory.SetValue(VerticalMenuItem.MenuContentProperty,
+                                         new FileInfo(imagePath).Name.Split('.')[0]);
+                Button button = new()
+                {
+                    Width = double.NaN,
+                    ContentTemplate = new DataTemplate
+                    {
+                        VisualTree = menuItemFactory
+                    }
+                };
+                MenuPanel.Children.Add(button);
+            }
+        }
+    }
+}
